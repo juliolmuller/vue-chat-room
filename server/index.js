f894076b3f3c7642f5b3c97cfd16bf1aa7439a21
Julio Muller <juliodolago@gmail.com>
2020-09-03 20:03:43 -0300

    Added assignment to environment variables

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const http = require('http').Server(app)
 const path = require('path')
 const io = require('socket.io')(http)
 
-const PORT = 3030
+const PORT = process.env.PORT || 3030
 
 app.get('/', (_req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
